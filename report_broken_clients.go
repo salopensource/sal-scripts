ad16d02902a8afa3de0217c9d6abbedd4fe0198f
Graham Gilbert <graham.gilbert@airbnb.com>
2018-01-18 15:12:40 -0800

    Make the Golang gods happy

--- report_broken_clients.go
+++ report_broken_clients.go
@@ -1,74 +1,74 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/groob/mackit/cfpref"
 )
 
-func serialNumber() string {
+func getSerialNumber() (string, error) {
 	out, err := exec.Command("/usr/sbin/ioreg", "-l").Output()
 	if err != nil {
-		return ""
+		log.Fatal(err)
 	}
 	for _, l := range strings.Split(string(out), "\n") {
 		if strings.Contains(l, "IOPlatformSerialNumber") {
 			s := strings.Split(l, " ")
 			t := strings.Replace(s[len(s)-1], "\"", "", -1)
-			return t
+			return t, nil
 		}
 	}
-	return ""
+	return "", errors.New("could not retrieve serial number")
 }
 
 func main() {
-	serial_number := serialNumber()
-	if serial_number == "" {
-		panic("Could not retrieve serial number")
+	serialNumber, err := getSerialNumber()
+	if err != nil {
+		log.Fatalf("Could not retrieve serial number")
 	}
-	sal_url := cfpref.CopyAppValue("ServerURL", "com.github.salopensource.sal")
+	salUrl := cfpref.CopyAppValue("ServerURL", "com.github.salopensource.sal")
 	key := cfpref.CopyAppValue("key", "com.github.salopensource.sal")
 
 	//Testing
-	// sal_url := "http://127.0.0.1:8000"
+	// salUrl := "http://127.0.0.1:8000"
 	// Now you all know the key for my test group on my computer.
 	// key := "2k4wohv0sopmzkfp5ocubal47iwds8ddz8h4w7kjbcdjmbyhlgiztdyj7e8up048l89u35vqt08l4jrrkmaegj0j9ba264g3r80geybog0e9e2wrik6jqow7wh4fb5az"
 
-	the_url := fmt.Sprintf("%v/checkin/", sal_url)
-	the_key := fmt.Sprintf("%v", key)
+	theUrl, err := url.Parse(fmt.Sprintf("%v", salUrl))
+	if err != nil {
+		log.Fatal(err)
+	}
+	theUrl.Path = "/checkin/"
+	theKey := fmt.Sprintf("%v", key)
 
 	v := url.Values{}
 	v.Set("broken_client", "True")
-	v.Set("key", the_key)
-	v.Set("serial", serial_number)
+	v.Set("key", theKey)
+	v.Set("serial", serialNumber)
 
-	req, err := http.NewRequest("POST", the_url, strings.NewReader(v.Encode()))
+	req, err := http.NewRequest("POST", theUrl.String(), strings.NewReader(v.Encode()))
 	if err != nil {
-		panic(fmt.Errorf("failed to create request: %s", err))
+		log.Fatalf("failed to create request: %s", err)
 	}
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	req.SetBasicAuth("sal", the_key)
+	req.SetBasicAuth("sal", theKey)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		panic(fmt.Errorf("failed to checkin: %s", err))
+		log.Fatalf("failed to checkin: %s", err)
 	}
 
 	defer resp.Body.Close()
 
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	bodyString := string(bodyBytes)
-
-	fmt.Println(bodyString)
+	io.Copy(os.Stdout, resp.Body)
 }
