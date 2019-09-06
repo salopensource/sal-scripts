2e968de3e6373c803e4161c2c664ec848905fb23
Shea Craig <sheagcraig@gmail.com>
2019-09-06 15:31:22 -0400

    Bump version.

--- payload/usr/local/sal/utils.py
+++ payload/usr/local/sal/utils.py
@@ -22,7 +22,7 @@ from Foundation import (kCFPreferencesAnyUser, kCFPreferencesCurrentHost, CFPref
 
 BUNDLE_ID = 'com.github.salopensource.sal'
 RESULTS_PATH = '/usr/local/sal/checkin_results.json'
-VERSION = '3.0.1'
+VERSION = '3.0.2'
 
 
 def sal_version():
