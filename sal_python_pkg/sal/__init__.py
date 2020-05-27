f03b92c2f52f26800bf2fa95ba6f2b7a9661e542
Shea Craig <sheagcraig@gmail.com>
2020-05-27 19:45:00 -0400

    Create generic pref getting mac util.

--- sal_python_pkg/sal/__init__.py
+++ sal_python_pkg/sal/__init__.py
@@ -1,4 +1,8 @@
 from sal.client import MacKeychainClient, SalClient, get_sal_client
-from sal.mac_utils import *
+try:
+    from sal.mac_utils import *
+except ImportError:
+    # Allow non-macOS to import safely.
+    pass
 from sal.utils import *
 from sal.version import __version__
