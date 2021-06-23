f14802d66d27ad3943ef38f6d32c24de96697992
Graham Gilbert <graham.gilbert@airbnb.com>
2021-06-23 07:39:45 -0700

    3.9.5

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 USE_PKGBUILD=1
 include luggage/luggage.make
 PACKAGE_VERSION:=$(shell sed -n -e '/^__version__/p' sal_python_pkg/sal/version.py | cut -d "\"" -f 2)
+include config.mk
+PB_EXTRA_ARGS+= --sign "${DEV_INSTALL_CERT}"
 TITLE=sal_scripts
-PACKAGE_NAME=sal_scripts
+PACKAGE_NAME=sal-scripts
 REVERSE_DOMAIN=com.github.salopensource
 PYTHONTOOLDIR=/tmp/relocatable-python-git
 PAYLOAD=\
@@ -13,11 +15,27 @@ PAYLOAD=\
 	pack-script-postinstall \
 	pack-python
 
+check_variables:
+ifndef DEV_INSTALL_CERT
+$(error "DEV_INSTALL_CERT" is not set)
+endif
+ifndef DEV_APP_CERT
+$(error "DEV_APP_CERT" is not set)
+endif
+ifndef APPLE_ACC_USER
+$(error "APPLE_ACC_USER" is not set)
+endif
+ifndef APPLE_ACC_PWD
+$(error "APPLE_ACC_PWD" is not set)
+endif
+
 clean-build:
 	@sudo rm -rf report_broken_client/build
 
 pack-report-broken-client: pack-sal-scripts clean-build
 	xcodebuild -project report_broken_client/report_broken_client.xcodeproj -configuration Release
+	/usr/libexec/PlistBuddy -c "Add :com.apple.security.cs.allow-unsigned-executable-memory bool true" ${TOOLSDIR}/entitlements.plist
+	codesign --force --deep --verbose -s "${DEV_APP_CERT}" --entitlements entitlements.plist report_broken_client/build/Release/report_broken_client
 	@sudo ${CP} report_broken_client/build/Release/report_broken_client ${WORK_D}/usr/local/munki/report_broken_client
 
 pack-sal-scripts: l_usr_local
@@ -45,5 +63,10 @@ build-python:
 	# we grok the GNU better.
 	@rm -rf "${PYTHONTOOLDIR}"
 	@git clone https://github.com/gregneagle/relocatable-python.git "${PYTHONTOOLDIR}"
-	@./build_python_framework.sh
+	@./build_python_framework.sh "${DEV_APP_CERT}"
 	@find ./Python.framework -name '*.pyc' -delete
+
+notarize:
+	@./notarize.sh "${APPLE_ACC_USER}" "${APPLE_ACC_PWD}"
+
+release: pkg notarize
--- build_python_framework.sh
+++ build_python_framework.sh
@@ -1,10 +1,31 @@
 #!/bin/zsh
+
 # Build script for Python 3 framework for Sal scripts
 TOOLSDIR=$(dirname "$0")
-PYTHON_VERSION=3.8.5
+PYTHON_VERSION=3.9.5
+PYTHON_SHORT=3.9
+MACOS_VERSION=11 # use 10.9 for non-universal
+DevApp=$1
 
 # build the framework
 /tmp/relocatable-python-git/make_relocatable_python_framework.py \
     --python-version "${PYTHON_VERSION}" \
     --pip-requirements requirements.txt \
-    --destination "${TOOLSDIR}"
+    --os-version "${MACOS_VERSION}" \
+    --destination "${TOOLSDIR}" \
+    --upgrade-pip
+
+# sign all the bits of python with our Apple Developer ID Installer: cert.
+find ${TOOLSDIR}/Python.framework -name '*.pyc' -delete
+find ${TOOLSDIR}/Python.framework/Versions/${PYTHON_SHORT}/lib/ -type f -perm -u=x -exec codesign --force --deep --verbose -s "$DevApp" {} \;
+find ${TOOLSDIR}/Python.framework/Versions/${PYTHON_SHORT}/bin/ -type f -perm -u=x -exec codesign --force --deep --verbose -s "$DevApp" {} \;
+find ${TOOLSDIR}/Python.framework/Versions/${PYTHON_SHORT}/lib/ -type f -name "*dylib" -exec codesign --force --deep --verbose -s "$DevApp" {} \;
+
+/usr/libexec/PlistBuddy -c "Add :com.apple.security.cs.allow-unsigned-executable-memory bool true" ${TOOLSDIR}/entitlements.plist
+
+codesign --force --options runtime --entitlements $TOOLSDIR/entitlements.plist --deep --verbose -s "$DevApp" $TOOLSDIR/Python.framework/Versions/${PYTHON_SHORT}/Resources/Python.app/
+codesign --force --deep --options runtime --entitlements $TOOLSDIR/entitlements.plist --deep --verbose -s "$DevApp" $TOOLSDIR/Python.framework/Versions/${PYTHON_SHORT}/bin/*
+codesign --force --deep --options runtime --entitlements $TOOLSDIR/entitlements.plist --deep --verbose -s "$DevApp" $TOOLSDIR/Python.framework/Versions/${PYTHON_SHORT}/lib/*
+codesign --force --deep --options runtime --entitlements $TOOLSDIR/entitlements.plist --deep --verbose -s "$DevApp" $TOOLSDIR/Python.framework/Versions/${PYTHON_SHORT}/python
+codesign --force --deep --verbose -s "$DevApp" $TOOLSDIR/Python.framework
+codesign --force --deep --options runtime --entitlements $TOOLSDIR/entitlements.plist --deep --verbose -s "$DevApp" $TOOLSDIR/Python.framework/python
